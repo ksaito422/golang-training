3b4cff05ad04e6fbda43174124d3e3f2e813dc84
saitooooooo <kitsit.39@gmail.com>
2022-07-28 21:15:41 +0900

    ゼロ値

---
+++ zero/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 初期値の確認
+	var i int
+	var j bool
+	var k string
+	var l *int
+	var m []string
+
+	fmt.Println("i =", i)
+	fmt.Println("j =", j)
+	fmt.Println("k =", k)
+	fmt.Println("l =", l)
+	fmt.Println("m =", m)
+}
