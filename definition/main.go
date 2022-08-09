1fde7cd69781d46e5ffecd6ca9c0b4d02d8c14c7
saitooooooo <kitsit.39@gmail.com>
2022-08-09 09:03:14 +0900

    組み込み型の拡張

--- definition/main.go
+++ definition/main.go
@@ -1,7 +1,18 @@
 package main
 
-import ()
+import (
+	"fmt"
+)
 
 func main() {
 	Reflect()
+
+	fmt.Println(HTTPStatus.String(200))
+	fmt.Println(HTTPStatus.String(401))
+	fmt.Println(HTTPStatus.String(402))
+	fmt.Println(HTTPStatus.String(403))
+	fmt.Println(NationalRoute.String(200))
+	fmt.Println(NationalRoute.String(401))
+	fmt.Println(NationalRoute.String(402))
+	fmt.Println(NationalRoute.String(403))
 }
