85af30a123364fc0f50e3d76ab28b761604dc2b5
saitooooooo <kitsit.39@gmail.com>
2022-08-13 10:48:01 +0900

    json decode

---
+++ json/decode.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type ip struct {
+	Origin string `json:"origin"`
+	URL    string `json:"url"`
+}
+
+func Decode() {
+	f, err := os.Open("ip.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var resp ip
+	if err := json.NewDecoder(f).Decode(&resp); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", resp)
+}
