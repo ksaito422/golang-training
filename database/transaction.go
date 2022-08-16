651fea0a8b14860fe3e72e5962c02eaad1303cbc
saitooooooo <kitsit.39@gmail.com>
2022-08-16 20:16:42 +0900

    WIP DB transaction動かないやつ

--- database/transaction.go
+++ database/transaction.go
@@ -23,3 +23,27 @@ func UpdateUser(db *sql.DB, userName, userID string) {
 	}
 	tx.Commit()
 }
+
+// トランザクションを制御するメソッド
+// アプリ開発者が本メソッドを使って、DMLのクエリを発行する
+func (t *txAdmin) Transaction(ctx context.Context, f func(ctx context.Context) (err error)) error {
+	tx, err := t.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	if err := f(ctx); err != nil {
+		return fmt.Errorf("transaction query failed: %w", err)
+	}
+	return tx.Commit()
+}
+
+func (s *Service) UpdateUser(ctx context.Context, userID, userName string) error {
+	updateFunc := func(ctx context.Context) error {
+		if _, err := s.tx.ExecContext(ctx, "UPDATE users SET user_name = $1 WHERE user_id = $2", userName, userID); err != nil {
+			return err
+		}
+		return nil
+	}
+	return s.tx.Transaction(ctx, updateFunc)
+}
