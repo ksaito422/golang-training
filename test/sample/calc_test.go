c54224797d327790d8d9835807f872569e9c0449
saitooooooo <kitsit.39@gmail.com>
2022-08-19 20:24:30 +0900

    Benchmark

--- test/sample/calc_test.go
+++ test/sample/calc_test.go
@@ -81,3 +81,48 @@ func setup() {
 func teardown() {
 	log.Println(">> テスト全体の実行後")
 }
+
+func BenchmarkCalc(b *testing.B) {
+	// テストケース
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		// 正常系
+		{
+			name: "足し算",
+			args: args{
+				a:        10,
+				b:        2,
+				operator: "+",
+			},
+			want:    12,
+			wantErr: false,
+		},
+		// 異常系
+		{
+			name: "不正な演算子を指定",
+			args: args{
+				a:        10,
+				b:        2,
+				operator: "?",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			got, err := Calc(tt.args.a, tt.args.b, tt.args.operator)
+			if (err != nil) != tt.wantErr {
+				b.Errorf("Calc() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				b.Errorf("Calc() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
