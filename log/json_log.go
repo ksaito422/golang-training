04a183459afec3b465f98f2d7a3caaeb6ce7e999
saitooooooo <kitsit.39@gmail.com>
2022-11-07 19:55:06 +0900

    feat: debugモードで出力するログ設定

--- log/json_log.go
+++ log/json_log.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func JsonLog() {
-	log.Printf("Hello World")
+	debug := flag.Bool("debug", false, "sets log level to debug")
+	flag.Parse()
 
 	// info以上のログのみ出力されるように設定
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
 
 	// それぞれのレベルで出力
 	log.Error().Msg("errorで出力")
 	log.Info().Msg("infoで出力")
-	log.Debug().Msgf("debugで出力: %v", "ただし出力されない")
+	log.Debug().Msgf("debugで出力: %v", "debugモードでのみ出力される")
 }
