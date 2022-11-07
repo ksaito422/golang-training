ad55c1fda2c50f77ea53e90d7b6f7c35cd374a24
saitooooooo <kitsit.39@gmail.com>
2022-11-07 20:19:08 +0900

    feat: ログ出力の整形

--- log/json_log.go
+++ log/json_log.go
@@ -3,17 +3,35 @@ package main
 import (
 	buildinerrors "errors"
 	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/pkgerrors"
 
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	// "github.com/rs/zerolog/log"
 )
 
 func JsonLog() {
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 	err := outer()
+
+	// 出力形式の設定
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
+	}
+	output.FormatMessage = func(i interface{}) string {
+		return fmt.Sprintf("***%s****", i)
+	}
+	output.FormatFieldName = func(i interface{}) string {
+		return fmt.Sprintf("%s:", i)
+	}
+
+	log := zerolog.New(output).With().Timestamp().Logger()
 
 	debug := flag.Bool("debug", false, "sets log level to debug")
 	flag.Parse()
